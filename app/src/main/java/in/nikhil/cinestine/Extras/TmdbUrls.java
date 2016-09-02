49090b96fd422cd983fd7d5ff9fe7b18e5f82b2d
Nikhil B <nkl07ba@gmail.com>
2016-09-03 03:50:32 +0530

    Removed apikey.

--- app/src/main/java/in/nikhil/cinestine/Extras/TmdbUrls.java
+++ app/src/main/java/in/nikhil/cinestine/Extras/TmdbUrls.java
@@ -8,12 +8,14 @@ public class TmdbUrls {
     public static String MOVIE_BASE_URL = "http://api.themoviedb.org/3/movie/";
     public static String SORT_POPULAR = "popular?";
     public static String SORT_TOP_RATED = "top_rated?";
-    public static String API_KEY = "api_key=609bbb466b647591bcd182c19afd5a2d";
+
     public static String TRAILERS = "/videos?";
     public static String REVIEWS = "/reviews?";
     public static String PAGE = "&page=";
     public static String YOUTUBE_URL = "http://www.youtube.com/watch?v=";
     public static String YOUTUBE_THUMB = "http://img.youtube.com/vi/";
     public static String YOUTUBE_MEDIUM_QUALITY = "/mqdefault.jpg";
+
+    public static String API_KEY = "api_key=\" Add key here\"";
 
 }
